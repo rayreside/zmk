dff4c5010b877946908646a856d1adbb2f4b361f
Ben Sun <86217481+bojuns@users.noreply.github.com>
2023-07-22 16:28:14 -0700

    DFS Generated Maze

--- app/boards/shields/nice_view/widgets/peripheral_status.c
+++ app/boards/shields/nice_view/widgets/peripheral_status.c
@@ -25,27 +25,20 @@ LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 #define LEN_FRAMES 94310
 #define LEN_DICT 2048
 #define FPS 15
-#define NUM_SAND 1654
+#define UPDATES_PER_FRAME 5
 #define BOARD_R 68
 #define BOARD_C 136
-#define SAND_W 48
-#define SAND_H 100
-#define C_OFFSET 18
-#define R_OFFSET 10
 #define FRAME_L (BOARD_R * BOARD_C / 8 + 8)
-
 static sys_slist_t widgets = SYS_SLIST_STATIC_INIT(&widgets);
 static const uint16_t ms_per_frame = 1000 / FPS;
-
-typedef enum { BLANK = 0, WALL, SAND, BARRIER } Pixel;
-
 struct peripheral_status_state {
     bool connected;
 };
 
-extern uint8_t board[BOARD_R][BOARD_C];
-extern uint8_t board_bits[FRAME_L];
-extern uint8_t sand_coords[NUM_SAND][2];
+static uint8_t board_bits[FRAME_L] = {
+    0xff, 0xff, 0xff, 0xff, /*Color of index 1*/
+    0x00, 0x00, 0x00, 0xff, /*Color of index 0*/
+};
 static uint32_t frame_counter;
 static uint32_t lcg;
 static lv_draw_img_dsc_t img_dsc;
@@ -56,12 +49,132 @@ K_WORK_DEFINE(anim_work, anim_work_handler);
 void anim_expiry_function() { k_work_submit(&anim_work); }
 #define FRAME_SIZE (ANIM_SIZE * ANIM_SIZE / 8 + 8)
 K_TIMER_DEFINE(anim_timer, anim_expiry_function, NULL);
-uint8_t new_frame[FRAME_SIZE] = {
-    0x00, 0x00, 0x00, 0xff, /*Color of index 0*/
-    0xff, 0xff, 0xff, 0xff  /*Color of index 1*/
-};
-static int8_t direction;
-static int8_t same_frames;
+#define MAZE_WIDTH 68
+#define MAZE_HEIGHT 34
+#define MAZE_AREA (MAZE_WIDTH * MAZE_HEIGHT / 4)
+static uint8_t maze[MAZE_HEIGHT][MAZE_WIDTH];
+static int16_t stack[MAZE_AREA][2];
+static uint16_t stack_top = 0;
+typedef enum { NORTH = 1, SOUTH = 2, EAST = 4, WEST = 8, WALL = 16, VISITED = 32 } Direction;
+static uint8_t perms[24][4] = {
+    {NORTH, SOUTH, EAST, WEST}, {SOUTH, NORTH, EAST, WEST}, {EAST, NORTH, SOUTH, WEST},
+    {NORTH, EAST, SOUTH, WEST}, {SOUTH, EAST, NORTH, WEST}, {EAST, SOUTH, NORTH, WEST},
+    {EAST, SOUTH, WEST, NORTH}, {SOUTH, EAST, WEST, NORTH}, {WEST, EAST, SOUTH, NORTH},
+    {EAST, WEST, SOUTH, NORTH}, {SOUTH, WEST, EAST, NORTH}, {WEST, SOUTH, EAST, NORTH},
+    {WEST, NORTH, EAST, SOUTH}, {NORTH, WEST, EAST, SOUTH}, {EAST, WEST, NORTH, SOUTH},
+    {WEST, EAST, NORTH, SOUTH}, {NORTH, EAST, WEST, SOUTH}, {EAST, NORTH, WEST, SOUTH},
+    {SOUTH, NORTH, WEST, EAST}, {NORTH, SOUTH, WEST, EAST}, {WEST, SOUTH, NORTH, EAST},
+    {SOUTH, WEST, NORTH, EAST}, {NORTH, WEST, SOUTH, EAST}, {WEST, NORTH, SOUTH, EAST}};
+void set_block_at(int16_t row, int16_t col) {
+    row = row * 2;
+    col = col * 2;
+    set_pixel_at(row, col);
+    set_pixel_at(row + 1, col);
+    set_pixel_at(row, col + 1);
+    set_pixel_at(row + 1, col + 1);
+}
+void set_pixel_at(int16_t row, uint16_t col) {
+    int16_t temp = row * BOARD_C + col;
+    int16_t byte_idx = (temp >> 3) + 8;
+    int8_t bit_idx = temp & 0x7;
+    board_bits[byte_idx] |= (0x80 >> bit_idx);
+}
+void push(int16_t row, int16_t col) {
+    stack[stack_top][0] = row;
+    stack[stack_top][1] = col;
+    maze[row][col] |= VISITED;
+    set_block_at(row, col);
+    stack_top++;
+}
+void pop(int16_t *row, int16_t *col) {
+    *row = stack[stack_top - 1][0];
+    *col = stack[stack_top - 1][1];
+    stack_top--;
+}
+void peek(int16_t *row, int16_t *col) {
+    *row = stack[stack_top - 1][0];
+    *col = stack[stack_top - 1][1];
+}
+int8_t x_offset(uint8_t dir) {
+    switch (dir) {
+    case EAST:
+        return 1;
+    case WEST:
+        return -1;
+    default:
+        return 0;
+    }
+}
+int8_t y_offset(uint8_t dir) {
+    switch (dir) {
+    case SOUTH:
+        return 1;
+    case NORTH:
+        return -1;
+    default:
+        return 0;
+    }
+}
+uint8_t opposite(uint8_t dir) {
+    switch (dir) {
+    case NORTH:
+        return SOUTH;
+    case EAST:
+        return WEST;
+    case SOUTH:
+        return NORTH;
+    case WEST:
+        return EAST;
+    }
+    return 0;
+}
+uint8_t make_maze_step() {
+    if (stack_top == 0) {
+        return 0;
+    }
+    int16_t row, col;
+    uint8_t *order = perms[lcg % 24];
+    lcg = lcg * 22695477 + 1;
+    do {
+        peek(&row, &col);
+        uint8_t val = maze[row][col];
+        for (int i = 0; i < 4; i++) {
+            uint8_t dir = order[i];
+            int16_t r_offset = y_offset(dir);
+            int16_t c_offset = x_offset(dir);
+            int16_t new_r = row + r_offset * 2;
+            int16_t new_c = (int16_t)col + c_offset * 2;
+            if (new_r < 0 || new_r >= MAZE_HEIGHT || new_c < 0 || new_c >= MAZE_WIDTH) {
+                continue;
+            }
+            if ((val & dir) || maze[new_r][new_c] & VISITED) {
+                continue;
+            }
+            maze[row][col] |= (dir | VISITED);
+            maze[row + r_offset][col + c_offset] |= VISITED;
+            maze[new_r][new_c] |= opposite(dir);
+            set_block_at(row, col);
+            set_block_at(row + r_offset, col + c_offset);
+            push(new_r, new_c);
+            return 1;
+        }
+        stack_top--;
+    } while (stack_top > 0);
+    return 0;
+}
+void init_maze() {
+    for (int r = 0; r < MAZE_HEIGHT; r++) {
+        for (int c = 0; c < MAZE_WIDTH; c++) {
+            maze[r][c] = WALL;
+        }
+    }
+    for (int i = 8; i < FRAME_L; i++) {
+        board_bits[i] = 0x0;
+    }
+    uint16_t start_r = lcg % MAZE_HEIGHT;
+    uint16_t start_c = (lcg >> 16) % MAZE_WIDTH;
+    push(start_r, start_c);
+}
 lv_img_dsc_t frame = {.header.cf = LV_IMG_CF_INDEXED_1BIT,
                       .header.always_zero = 0,
                       .header.reserved = 0,
@@ -70,85 +183,19 @@ lv_img_dsc_t frame = {.header.cf = LV_IMG_CF_INDEXED_1BIT,
                       .data_size = FRAME_L,
                       .data = board_bits};
 void anim_work_handler(struct k_work *work) {
-    for (int s_left = NUM_SAND - 1; s_left > 0; s_left--) {
-        // LCG to pick index of sand grain to shuffle
-        lcg = lcg * 22695477 + 1;
-        // Fisher-Yates shuffle the sand array
-        uint16_t sand_idx = lcg % s_left;
-        uint8_t *sand_selected = sand_coords[sand_idx];
-        uint8_t *sand_current = sand_coords[s_left];
-        // Swapped element is to be updated
-        uint8_t row = sand_selected[1];
-        uint8_t col = sand_selected[0];
-        sand_selected[0] = sand_current[0];
-        sand_selected[1] = sand_current[1];
-        bool update = false;
-        uint8_t nr, nc;
-        if (!board[row][col + direction]) {
-            nr = row;
-            nc = col + direction;
-            update = true;
-        } else {
-            bool can_go_left = !board[row - 1][col];
-            bool can_go_right = !board[row + 1][col];
-            nc = col;
-            if (can_go_left & can_go_right) {
-                if (lcg >> 16 & 0x1) {
-                    nr = row - 1;
-                } else {
-                    nr = row + 1;
-                }
-                update = true;
-            } else if (can_go_left) {
-                nr = row - 1;
-                update = true;
-            } else if (can_go_right) {
-                nr = row + 1;
-                update = true;
+    for (int i = 0; i < UPDATES_PER_FRAME; i++) {
+        if (!make_maze_step()) {
+            frame_counter++;
+            if (frame_counter == FPS) {
+                frame_counter = 0;
+                init_maze();
             }
-        }
-        if (update) {
-            // Updating the game board
-            board[row][col] = BLANK;
-            board[nr][nc] = SAND;
-            // Calculating byte coordinates and value of original pixel
-            uint16_t temp = row * BOARD_C + col;
-            uint16_t byte_idx = (temp >> 3) + 8;
-            uint8_t bit_idx = temp & 0x7;
-            board_bits[byte_idx] ^= (0x80 >> bit_idx);
-            // Calculating byte coordinates and value of new pixel
-            temp = nr * BOARD_C + nc;
-            byte_idx = (temp >> 3) + 8;
-            bit_idx = temp & 0x7;
-            board_bits[byte_idx] ^= (0x80 >> bit_idx);
-            // Updating sand
-            sand_current[0] = nc;
-            sand_current[1] = nr;
-        } else {
-            sand_current[0] = col;
-            sand_current[1] = row;
+            break;
         }
     }
     lv_canvas_draw_img(video, 0, 0, &frame, &img_dsc);
     // Set timer to go off when animation finishes
     k_timer_start(&anim_timer, K_MSEC(ms_per_frame), K_MSEC(ms_per_frame));
-    for (int r = 32; r < 36; r++) {
-        if (board[r][67] == board[r][68]) {
-            same_frames = 0;
-            return;
-        }
-    }
-    for (int r = 32; r < 35; r++) {
-        if (board[r][67] != board[r + 1][67] || board[r][68] != board[r + 1][68]) {
-            same_frames = 0;
-            return;
-        }
-    }
-    same_frames++;
-    if (same_frames > 15) {
-        same_frames = 0;
-        direction *= -1;
-    }
 }
 
 static void draw_top(lv_obj_t *widget, lv_color_t cbuf[], struct status_state state) {
@@ -243,9 +290,8 @@ int zmk_widget_status_init(struct zmk_widget_status *widget, lv_obj_t *parent) {
     lv_draw_img_dsc_init(&img_dsc);
     frame_counter = 0;
     lcg = sys_rand32_get();
-    direction = 1;
-    same_frames = 0;
-    // Starting animation timer
+    init_maze();
+    //  Starting animation timer
     k_timer_start(&anim_timer, K_MSEC(10), K_MSEC(10));
     return 0;
 }
